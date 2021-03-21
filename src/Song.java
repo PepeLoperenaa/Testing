e48a8b3400720a4bab9f2678802555159c6fd9c6
Marek <marek.stryjenski@gmail.com>
2021-03-21 11:55:54 +0100

    refactored play song method

--- src/Song.java
+++ src/Song.java
@@ -1,12 +1,45 @@
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+
 public class Song {
     private String songName;
     private String fileDirectory;
 
+    /**
+     * The song that user wants to play has to be a .wav file and must be inside songs directory
+     * @param songName name of the song
+     */
     public Song(String songName) {
         this.songName = songName;
-        this.fileDirectory = fileDirectory;
+
     }
 
+    public void playSong(){
+        try {
+            File soundFile = new File("songs/"+this.songName+".wav"); //com.nhlstenden.Sound file
+
+            //Getting AudioInputStream
+            //Can be thrown IOException и UnsupportedAudioFileException
+            AudioInputStream ais = AudioSystem.getAudioInputStream(soundFile);
+
+            //Getting realization of interface Clip
+            //Can be thrown LineUnavailableException
+            Clip clip = AudioSystem.getClip();
+
+            //Uploading sound to sound stream Clip
+            //Can be thrown IOException и LineUnavailableException
+            clip.open(ais);
+
+            clip.setFramePosition(0); //Setting pointer on start
+            clip.start();
+
+            Thread.sleep(clip.getMicrosecondLength()/1000);
+        }
+        catch (InterruptedException exc) {} catch (LineUnavailableException | IOException | UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        }
+    }
     public String getSongName() {
         return songName;
     }
--- src/main.java
+++ src/main.java
@@ -1,7 +1,10 @@
 public class main {
     public static void main(String[] args) {
         Radio r1 = new Radio();
+        Song s=new Song("car");
+        s.playSong();
 
-        r1.playStatic();
+//        r1.playStatic();
+
     }
 }
