3ec7d0821f292c2a8d833704ca96f827813996c8
Pepe Loperena <43403622+PepeLoperenaa@users.noreply.github.com>
2021-03-21 12:08:44 +0000

    Working on methods

--- src/main.java
+++ src/main.java
@@ -3,8 +3,11 @@ public class main {
         Radio r1 = new Radio();
 //        Song car = new Song("car");
         Song example = new Song("laPlacita");
-
         RadioStation rmfFm = new RadioStation("RmfFm", 86.75);
+        RadioStation zet = new RadioStation("zet", 90);
+//        r1.addRadioStation(rmfFm);
+//        r1.addRadioStation(zet);
+
 
 //        rmfFm.addSong(car);
         rmfFm.addSong(example);
